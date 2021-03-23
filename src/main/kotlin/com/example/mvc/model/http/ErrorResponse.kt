a4b1ac294905ff7708bebf7935b6e8fa8754eb29
lee.jun.su <qkfhtit@gmail.com>
2021-03-23 11:17:11 +0900

    2021-03-23

--- src/main/kotlin/com/example/mvc/model/http/ErrorResponse.kt
+++ src/main/kotlin/com/example/mvc/model/http/ErrorResponse.kt
@@ -10,13 +10,18 @@ data class ErrorResponse (
 
    @field:JsonProperty("http_status")
    var httpStatus:String? = null,
+
+   @field:JsonProperty("http_method")
+   var httpMethod:String? = null,
    var message:String? = null,
    var path:String? = null,
    var timestamp:LocalDateTime? = null,
    var errors:MutableList<Error>? = mutableListOf()
-        )
+
+)
 
 data class Error(
     var field:String? = null,
-    var message:String? = null
+    var message:String? = null,
+    var value:Any? = null
 )
